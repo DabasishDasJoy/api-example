4c70be2a0e03fd5fa7ef64ce7419a3060a361a49
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-08-27 23:27:39 +0600

    displayed country details/dynamic placeholder

--- js/users.js
+++ js/users.js
@@ -8,7 +8,7 @@ const displayData = (users) => {
     for (const result of users.results) {
         const div = document.createElement('div');
         div.innerHTML = `
-        <div>
+        <div class="div">
             <h2>${result.name.title} ${result.name.first} ${result.name.last}</h2>
             <p>Email: ${result.email}</p>
             <p>${result.location.city}, ${result.location.country}</p>
